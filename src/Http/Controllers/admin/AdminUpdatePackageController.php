f25b35fc9dd9d41e0e5d9fe343f6940c5902834c
Gianluca Mongelli <g.mongelli@intentsrl.it>
2020-12-29 19:41:48 +0100

    algorithm to update package

--- src/Http/Controllers/admin/AdminUpdatePackageController.php
+++ src/Http/Controllers/admin/AdminUpdatePackageController.php
@@ -10,7 +10,7 @@ use Mongi\Mongicommerce\Http\Controllers\Controller;
 class AdminUpdatePackageController extends Controller
 {
     public function update(){
-       $dd = shell_exec('composer update 2>&1 mongi/mongicommerce');
+       $dd = shell_exec('cd .. && composer update 2>&1 mongi/mongicommerce');
         echo '<pre>';
         var_dump($dd);
         echo '</pre>';
