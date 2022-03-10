397f5e7018d68fd4ec437fe75c1313e3d2dd602b
Pasquale Guadagnuolo <pasquale.guadagnuolo@hotmail.com>
2022-03-10 17:41:03 +0100

    aggiunta dettagli ordini

--- resources/views/admin/pages/orders/order_details.blade.php
+++ resources/views/admin/pages/orders/order_details.blade.php
@@ -8,200 +8,220 @@
 @section('subheader')
 @endsection
 @section('content')
-<div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Informazioni Ordine</h3>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-2">
-                        <label> ID Ordine:</label>
-                        <input type="text" value="{{$order->id}}" class="form-control" readonly>
-                    </div>
-                    <div class="col-md-3">
-                        <label> Prezzo Spedizione:</label>
-                        <input type="text" value="@money($order->shipping_price)" class="form-control" readonly>
-                    </div>
-                    <div class="col-md-2">
-                        <label> Prezzo Ordine:</label>
-                        <input type="text" value="@money($order->total_price - $order->shipping_price)"
-                            class="form-control" readonly>
-                    </div>
-                    <div class="col-md-2">
-                        <label> Peso ordine:</label>
-                        <input type="text" value="{{$order->order_weight}}" class="form-control" readonly>
-                    </div>
-                    <div class="col-md-3">
-                        <label> Prezzo totale:</label>
-                        <input type="text" value="@money($order->total_price)" class="form-control" readonly>
-                    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Informazioni Ordine</h3>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <label> Note spedizione:</label>
-                        <textarea class="form-control" disabled>{{$order->note_delivery}}</textarea>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <label> ID Ordine:</label>
+                            <input type="text" value="{{$order->id}}" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label> Prezzo Spedizione:</label>
+                            <input type="text" value="@money($order->shipping_price)" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-2">
+                            <label> Prezzo Ordine:</label>
+                            <input type="text" value="@money($order->total_price - $order->shipping_price)"
+                                   class="form-control" readonly>
+                        </div>
+                        <div class="col-md-2">
+                            <label> Peso ordine:</label>
+                            <input type="text" value="{{$order->order_weight}}" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label> Prezzo totale:</label>
+                            <input type="text" value="@money($order->total_price)" class="form-control" readonly>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <label> Note ordine:</label>
-                        <textarea class="form-control" disabled>{{$order->note_order}}</textarea>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label> Note spedizione:</label>
+                            <textarea class="form-control" disabled>{{$order->note_delivery}}</textarea>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <label> Stato ordine:</label>
-                        <select onchange="changeStatus(this,{{$order->id}})" class="form-control" name="" id="">
-                            @foreach($statuses as $status)
-                            <option {{$status->id == $order->status_id ? 'selected':''}} value="{{$status->id}}">
-                                {{$status->name}}</option>
-                            @endforeach
-                        </select>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label> Note ordine:</label>
+                            <textarea class="form-control" disabled>{{$order->note_order}}</textarea>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <label> Codice di spedizione:</label>
-                        <input type="text" id="id_shipping" value="{{$order->id_shipping}}" class="form-control">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label> Stato ordine:</label>
+                            <select onchange="changeStatus(this,{{$order->id}})" class="form-control" name="" id="">
+                                @foreach($statuses as $status)
+                                    <option {{$status->id == $order->status_id ? 'selected':''}} value="{{$status->id}}">
+                                        {{$status->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <label> Gift card utilizzata:</label>
-                        <input type="text" id="id_shipping" @if($order->gift_code_id != null) value="si" @else value="no" @endif class="form-control" readonly>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label> Codice di spedizione:</label>
+                            <input type="text" id="id_shipping" value="{{$order->id_shipping}}" class="form-control">
+                        </div>
                     </div>
-                    <div class="col-md-4">
-                        <label> Nome gift card:</label>
-                        <input type="text" id="id_shipping" @if($order->gift_code_id != null) value="{{$product_item_gift->name}}" @else value="-" @endif class="form-control" readonly>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label> Gift card utilizzata:</label>
+                            <input type="text" id="id_shipping" @if($order->gift_code_id != null) value="si" @else value="no" @endif class="form-control" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label> Nome gift card:</label>
+                            <input type="text" id="id_shipping" @if($order->gift_code_id != null) value="{{$product_item_gift->name}}" @else value="-" @endif class="form-control" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label> Valore gift card:</label>
+                            <input type="text" id="id_shipping" @if($order->gift_code_id != null) value="€ {{$product_item_gift->price}}" @else value="-" @endif class="form-control" readonly>
+                        </div>
                     </div>
-                    <div class="col-md-4">
-                        <label> Valore gift card:</label>
-                        <input type="text" id="id_shipping" @if($order->gift_code_id != null) value="€ {{$product_item_gift->price}}" @else value="-" @endif class="form-control" readonly>
-                    </div>
-                </div>
 
 
-            </div>
-            <div class="card-footer">
-                <button onclick="saveShippingCode()" class="btn btn-primary">Salva</button>
+                </div>
+                <div class="card-footer">
+                    <button onclick="saveShippingCode()" class="btn btn-primary">Salva</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<hr>
-<div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Informazioni cliente</h3>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-2">
-                        <label> ID cliente:</label>
-                        <input type="text" id="id" value="{{$cliente->id}}" class="form-control" readonly>
-                    </div>
-                    <div class="col-md-3">
-                        <label>Nome:</label>
-                        <input type="text" id="nome" value="{{$cliente->first_name}}" class="form-control" readonly>
-                    </div>
-                    <div class="col-md-3">
-                        <label>Cognome:</label>
-                        <input type="text" id="cognome" value="{{$cliente->last_name}}" class="form-control" readonly>
-                    </div>
-                    <div class="col-md-4">
-                        <label>Codice fiscale:</label>
-                        <input type="text" id="cf" value="{{$cliente->cf}}" class="form-control" readonly>
-                    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Informazioni cliente</h3>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <label>Email:</label>
-                        <input type="text" id="email" class="form-control" value="{{$cliente->email}}" readonly>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <label> ID cliente:</label>
+                            <input type="text" id="id" value="{{$cliente->id}}" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>Nome:</label>
+                            <input type="text" id="nome" value="{{$cliente->first_name}}" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>Cognome:</label>
+                            <input type="text" id="cognome" value="{{$cliente->last_name}}" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label>Codice fiscale:</label>
+                            <input type="text" id="cf" value="{{$cliente->cf}}" class="form-control" readonly>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <label>Ragione Sociale:</label>
-                        <input type="text" id="ragione_sociale" class="form-control" value="{{$cliente->company}}" readonly>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label>Email:</label>
+                            <input type="text" id="email" class="form-control" value="{{$cliente->email}}" readonly>
+                        </div>
                     </div>
-                    <div class="col-md-4">
-                        <label>Partita IVA:</label>
-                        <input type="text" id="piva" class="form-control" value="{{$cliente->piva}}" readonly>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label>Ragione Sociale:</label>
+                            <input type="text" id="ragione_sociale" class="form-control" value="{{$cliente->company}}" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label>Partita IVA:</label>
+                            <input type="text" id="piva" class="form-control" value="{{$cliente->piva}}" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label>Codice fatturazione elettronica:</label>
+                            <input type="text" id="ipa" class="form-control" value="{{$cliente->ipa}}" readonly>
+                        </div>
                     </div>
-                    <div class="col-md-4">
-                        <label>Codice fatturazione elettronica:</label>
-                        <input type="text" id="ipa" class="form-control" value="{{$cliente->ipa}}" readonly>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-3">
-                        <label>Citta:</label>
-                        <input type="text" id="fax" class="form-control" value="{{$cliente->city}}" readonly>
-                    </div>
-                    <div class="col-md-3">
-                        <label>Indirizzo:</label>
-                        <input type="text" id="indirizzo" class="form-control" value="{{$cliente->address}}" readonly>
-                    </div>
-                    <div class="col-md-3">
-                        <label>Cap:</label>
-                        <input type="text" id="cap" class="form-control" value="{{$cliente->cap}}" readonly>
-                    </div>
-                    <div class="col-md-3">
-                        <label>Telefono:</label>
-                        <input type="text" id="telefono" class="form-control" value="{{$cliente->telephone}}" readonly>
-                    </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <label>Citta:</label>
+                            <input type="text" id="fax" class="form-control" value="{{$cliente->city}}" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>Indirizzo:</label>
+                            <input type="text" id="indirizzo" class="form-control" value="{{$cliente->address}}" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>Cap:</label>
+                            <input type="text" id="cap" class="form-control" value="{{$cliente->cap}}" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>Telefono:</label>
+                            <input type="text" id="telefono" class="form-control" value="{{$cliente->telephone}}" readonly>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
     </div>
-</div>
-<hr>
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Prodotti acquistati dal cliente</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                    <thead>
+    <hr>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Prodotti acquistati dal cliente</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
                         <tr>
                             <th>id</th>
                             <th>immagine</th>
                             <th>Nome</th>
+                            <th>Dettagli</th>
                             <th>Numero prodotti</th>
                             <th>prezzo</th>
                             <th>Categoria</th>
                         </tr>
-                    </thead>
-                    <tbody>
+                        </thead>
+                        <tbody>
 
                         @foreach($products as $product)
-                        <tr>
-                            <td>{{$product->id}}</td>
-                            <td><img width="70" src="{{asset($product->image)}}"></td>
-                            <td>{{$product->name}}</td>
-                            <td><span class="badge badge-warning">{{$product->pivot->number_products}}</span></td>
-                            <td>@money($product->price)</td>
-                            <td>{{$product->category->name}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
+                            <tr>
+                                <td>{{$product->id}}</td>
+                                <td><img width="70" src="{{asset($product->image)}}"></td>
+                                <td>{{$product->name}}</td>
+                                <td>
+                                    @if($product->pivot != null)
+                                        @foreach($products_item_id as $product_item)
+                                            @if($product_item->product_item_id == $product->pivot->product_item_id)
+                                                @foreach($details as $detail)
+                                                    @if ($detail->id == $product_item->product_detail_id)
+                                                        {{$detail->name}}:
+                                                    @endif
+                                                @endforeach
+                                                @foreach($details_value as $detail_value)
+                                                    @if ($detail_value->id == $product_item->product_detail_value_id)
+                                                        {{$detail_value->value}}<br>
+                                                    @endif
+                                                @endforeach
+                                            @endif
 
-                </table>
+                                        @endforeach
+                                    @endif
+                                </td>
+                                <td><span class="badge badge-warning">{{$product->pivot->number_products}}</span></td>
+                                <td>@money($product->price)</td>
+                                <td>{{$product->category->name}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+
+                    </table>
+                </div>
+                <!-- /.card-body -->
             </div>
-            <!-- /.card-body -->
+            <!-- /.card -->
         </div>
-        <!-- /.card -->
+        <!-- /.col -->
     </div>
-    <!-- /.col -->
-</div>
 @endsection
 @section('js')
 @endsection
--- src/Http/Controllers/shop/ShopOrderDetailsController.php
+++ src/Http/Controllers/shop/ShopOrderDetailsController.php
@@ -2,8 +2,11 @@
 namespace Mongi\Mongicommerce\Http\Controllers\shop;
 use Illuminate\Support\Facades\Auth;
 use Mongi\Mongicommerce\Http\Controllers\Controller;
+use Mongi\Mongicommerce\Models\Detail;
+use Mongi\Mongicommerce\Models\DetailValue;
 use Mongi\Mongicommerce\Models\Order;
 use Mongi\Mongicommerce\Models\Product;
+use Mongi\Mongicommerce\Models\ProductItemDetail;
 
 
 class ShopOrderDetailsController extends Controller
@@ -13,6 +16,11 @@ class ShopOrderDetailsController extends Controller
         $order = Order::find($order_id);
         $products_items = $order->products;
         $product_bool = array();
+
+        $products_item_id = ProductItemDetail::all();
+        $details = Detail::all();
+        $details_value = DetailValue::all();
+
         foreach ($products_items as $product_item) {
             $product_id = $product_item->product_id;
             $product = Product::find($product_id);
@@ -28,6 +36,6 @@ class ShopOrderDetailsController extends Controller
                 $show_code = true;
             }
         }
-        return view('mongicommerce.pages.user_order_details', compact('products_items', 'user', 'product_bool', 'order', 'show_code'));
+        return view('mongicommerce.pages.user_order_details', compact('products_items', 'user', 'product_bool', 'order', 'show_code','products_item_id','details','details_value'));
     }
 }
